c33215646e319cda945703c24a3e6bad6458aa74
Danack <Danack@basereality.com>
2015-08-22 13:12:17 +0000

    Refactoring code to be re-usable libraries.

--- src/Room11/Caching/LastModifiedStrategy.php
+++ src/Room11/Caching/LastModifiedStrategy.php
@@ -8,5 +8,11 @@ interface LastModifiedStrategy
     const CACHING_REVALIDATE = 'caching.revalidate';
     const CACHING_TIME = 'caching.time';
 
+    /**
+     * Gets the appropriate caching headers based on the last modified time
+     * 
+     * @param $lastModified
+     * @return mixed
+     */
     public function getHeaders($lastModified);
 }
